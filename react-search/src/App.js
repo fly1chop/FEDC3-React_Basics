090c8f3714ccf34b84666b1c4c7e08f65af0e20f
Jane Ro <janero95@gmail.com>
2022-12-20 00:47:33 +0900

    feat: show first 10 results from search input

--- react-search/src/App.js
+++ react-search/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import EmojiList from './components/EmojiList/index.js';
+import Header from './components/Header/index.js';
+import SearchBox from './components/SearchBox/index.js';
+import emojiData from './data/emoji.json';
 
 function App() {
+  const [keyword, setKeyword] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <SearchBox onSearch={setKeyword} />
+      <EmojiList emojis={emojiData} keyword={keyword} />
     </div>
   );
 }
