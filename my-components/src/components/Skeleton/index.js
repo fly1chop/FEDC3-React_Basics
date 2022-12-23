78cd6e84d0c5b4c5a0bc91516008575381f2df1d
Jane Ro <janero95@gmail.com>
2022-12-24 00:58:01 +0900

    feat: Skeleton component

---
+++ my-components/src/components/Skeleton/index.js
@@ -0,0 +1,11 @@
+import Box from './Box.js';
+import Circle from './Circle.js';
+import Paragraph from './Paragraph.js';
+
+const Skeleton = {
+  Box,
+  Circle,
+  Paragraph,
+};
+
+export default Skeleton;
---
+++ my-components/src/stories/Skeleton.stories.js
@@ -0,0 +1,30 @@
+import Skeleton from '../components/Skeleton/index.js';
+
+export default {
+  title: 'Component/Skeleton',
+};
+
+export const Box = args => <Skeleton.Box {...args} />;
+Box.argTypes = {
+  width: { defaultValue: 200, control: 'number' },
+  height: { defaultValue: 100, control: 'number' },
+};
+
+export const Circle = args => <Skeleton.Circle {...args} />;
+Circle.argTypes = {
+  size: { defaultValue: 100, control: 'number' },
+};
+
+export const Paragraph = args => <Skeleton.Paragraph {...args} />;
+Paragraph.argTypes = {
+  line: { defaultValue: 3, control: 'number' },
+};
+
+export const Sample = () => {
+  return (
+    <>
+      <Skeleton.Circle size={60} />
+      <Skeleton.Paragraph line={5} />
+    </>
+  );
+};
